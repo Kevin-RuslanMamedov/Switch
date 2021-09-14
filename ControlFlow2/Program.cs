640b554a1bbc93382677532d6378aae79ad980f2
Opilane TTHK <opilane@tthk.ee>
2021-09-14 10:42:00 +0300

    add switch

---
+++ ControlFlow2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ControlFlow2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere! Sisesta PIN");
+            int Pin = Convert.ToInt32(Console.ReadLine());
+
+            if (Pin == 1234)
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Vale PIN, proovi uuesti.");
+            }
+
+        }
+    }
+}
---
+++ InstagramAgeCheck/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InstagramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda vanus!");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            if (Age >= 13)
+            {
+                Console.WriteLine("Tere tulemast! Sa oled piisavalt vana");
+            }
+            else
+            {
+                Console.WriteLine("Sa ei ole piisavalt vana");
+            }
+
+        }
+    }
+}
